a4c54f9d4cf26333d8e74c2f9499e4a859f51898
BeCK8Pi <khuntip1207@gmail.com>
2024-09-21 13:39:12 +0700

    fsh

--- src/app/api/enrollments/route.ts
+++ src/app/api/enrollments/route.ts
@@ -30,7 +30,7 @@ export const GET = async (request: NextRequest) => {
     studentId = (<Payload>payload).studentId;
 
     //read role information from "payload" here (just one line code!)
-    //role = ...
+    role = (<Payload>payload).role;
   } catch {
     return NextResponse.json(
       {
@@ -46,6 +46,15 @@ export const GET = async (request: NextRequest) => {
   //     ok: true,
   //     enrollments: null //replace null with enrollment data!
   // }
+  if(role === "ADMIN"){
+    return NextResponse.json(
+      {
+        ok: true,
+        enrollment: DB.enrollments,
+      },
+      { status: 200 },
+    );
+  }
 
   const courseNoList = [];
   for (const enroll of DB.enrollments) {
@@ -85,7 +94,7 @@ export const POST = async (request: NextRequest) => {
     studentId = (<Payload>payload).studentId;
 
     //read role information from "payload" here (just one line code!)
-    //role = ...
+    role = (<Payload>payload).role;
   } catch {
     return NextResponse.json(
       {
@@ -104,6 +113,15 @@ export const POST = async (request: NextRequest) => {
   //   },
   //   { status: 403 }
   // );
+  if(role === "ADMIN"){
+    return NextResponse.json(
+      {
+        ok: true,
+        message: "Only Student can access this API route",
+      },
+      { status: 403 },
+    );
+  }
 
   //read body request
   const body = await request.json();
@@ -159,9 +177,37 @@ export const POST = async (request: NextRequest) => {
 
 export const DELETE = async (request: NextRequest) => {
   //check token
+  const rawAuthHeader = headers().get("authorization");
+
+  if(!rawAuthHeader || !rawAuthHeader.startsWith("Bearer ")){
+    return NextResponse.json(
+      {
+        ok: false,
+        messaage: "Authorization header is required"
+      },
+      { status: 401 },
+    )
+  }
   //verify token and get "studentId" and "role" information here
+  const token = rawAuthHeader.split(" ")[1];
+  const secret = process.env.JWT_SECRET || "This is my special secret";
+
   let studentId = null;
   let role = null;
+
+  try{
+    const payload = jwt.verify(token, secret);
+    studentId = (<Payload>payload).studentId;
+    role = (<Payload>payload).role;
+  }  catch{
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Invaid token",
+      },
+      { status: 401 },
+    )
+  }
 
   //if role is "ADMIN", send the following response
   // return NextResponse.json(
@@ -171,6 +217,15 @@ export const DELETE = async (request: NextRequest) => {
   //   },
   //   { status: 403 }
   // );
+  if(role === "ADMIN"){
+    return NextResponse.json(
+      {
+        ok: true,
+        message: "Only Student can access this API route",
+      },
+      { status: 403 },
+    )
+  }
 
   //get courseNo from body and validate it
   const body = await request.json();
